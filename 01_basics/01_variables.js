850890842bf9f34b8f07a2ce47b6e2ec52f0b950
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-09-28 06:41:43 +0000

    datatypes code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12244
+let accountEmail = "scanlain23@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+
+// accountId = 3545 //not allowed
+
+accountEmail = "mycode341@gmail.com"
+accountPassword = "98765"
+accountCity = "Banglore"
+
+console.log(accountId)
+console.table([accountId, accountPassword, accountCity, accountEmail])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let naav = "scanlain"
+let age = 24
+let isEmployed = false;
+
+console.table([naav, age, isEmployed])
+console.log(typeof null) //it is of object type(Some say this a error of JS)
+console.log(typeof undefined) // it has own type undefined
