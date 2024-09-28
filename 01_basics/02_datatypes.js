ceb558753dc7364e086ea4f3c3f691826f3018af
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-09-28 07:50:22 +0000

    string comment added

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,6 +2,8 @@ let naav = "scanlain"
 let age = 24
 let isEmployed = false;
 
+//string has length of maximum 2^53 - 1 elements
+
 console.table([naav, age, isEmployed])
 console.log(typeof null) //it is of object type(Some say this a error of JS)
 console.log(typeof undefined) // it has own type undefined
