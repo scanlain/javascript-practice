cffb35bc59269b50e31cde375602b3d89f729d78
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-09-30 01:39:49 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33
+//console.log(typeof score)
+
+/* 
+"33" -> 33 
+"33ab" -> NaN
+null -> 0
+true -> 1
+false -> 0
+undefined -> NaN 
+*/
+
+let isLogin = undefined
+let booleanLogIn = Boolean(isLogin)
+console.log(booleanLogIn)
+
+/*
+1 -> true
+0 -> false
+"scanlain" -> true
+"" -> false;
+null, undefined -> false
+*/
