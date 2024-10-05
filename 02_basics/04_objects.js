d2d6abf0684ba334d6bad2c1b36bf33f49d127d7
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-10-05 03:56:28 +0000

    objects info

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+let user = {
+    city : "indore",
+    age : 24,
+    fullName : {
+        firstName : "Tonny",
+        lastName : "Stark"
+    }
+}
+
+// console.log(user.fullName.firstName)
+// console.log(user)
+
+let obj1 = {1:"One", 2:"Two", 3:"Three"}
+let obj2 = {4:"Four", 5:"Five", 6: "Six"}
+
+// let mergedObj = {...obj1, ...obj2}
+// console.log(mergedObj)
+
+// let obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3)
+// console.log(Object.keys(obj1)) //returns a array of keys
+// console.log(Object.values(obj1)) //returns a array of values
+// console.log(Object.entries(obj1)) //returns a array of array of each pair
+
+console.log(obj2.hasOwnProperty(5))
