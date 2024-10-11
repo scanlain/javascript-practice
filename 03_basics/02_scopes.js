279959203b5e0c7437d80e39ae038528b4251180
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-10-11 09:58:08 +0000

    scope and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,6 +1,6 @@
-let a = 10
-const b = 20
-var c = 40
+// let a = 10
+// const b = 20
+// var c = 40
 
 {
      a = 100
@@ -8,4 +8,30 @@ var c = 40
     c = 400
 }
 
-console.log(a, b, c)
+// console.log(a, b, c)
+
+function one(){
+    const a = 10
+    // console.log(b)
+    function two(){
+        const b = 200
+        console.log(a)
+    }
+
+    
+    two()  
+}
+
+// one()
+
+
+//This is a normal function
+function one(num){
+    return num + 1
+}
+
+
+//This is a expression
+const addTwo = function(num) {
+    return num + 2
+}
