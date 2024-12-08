09c792f682cc73f85876ec701989ae8524491c00
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-12-08 01:11:26 +0000

    Comparison

---
+++ Datatypes_&_Conversions/comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+//gives the boolean values
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+//the reason is == and comparison operator works differently. Comparison op converts null to 0.
+
+// console.log(undefined == 0) //false
+// console.log(undefined > 0) //false
+// console.log(undefined >= 0) //false
+
+// == and ===
+//=== checks value and its datatypes too
+console.log("2" === 2)
