f0a89d9415ff392171748994ead56291920946ca
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2024-12-10 02:31:09 +0000

    Math methods

---
+++ Nums_&_Maths/Math.js
@@ -0,0 +1,26 @@
+// console.log(Math)
+
+//console.log(Math.abs(-4)) //converts -ve to +ve but no vice versa
+
+//console.log(Math.round(9.3)) //gives the nearest integer
+
+//console.log(Math.ceil(4.3)) //always returns top value
+
+//console.log(Math.floor(9.9)) //always returns the lower value
+
+//console.log(Math.PI.toFixed(3)) 
+
+// console.log(Math.max(3, -1, 10))
+
+// console.log(Math.min(-1, -2, -3, -4))
+
+// console.log(Math.pow(8,2))
+
+// console.log(Math.sqrt(81))
+
+// console.log(Math.floor(Math.random()*100 + 1))
+
+let max = 10
+let min = 30
+
+// console.log(Math.floor(Math.random()*(max - min + 1)) + min)
